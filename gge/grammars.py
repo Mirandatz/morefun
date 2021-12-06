0a3584eba96ee4d35dbe9c0a7e96a51acede226f
Mirandatz <Mirandatz@users.noreply.github.com>
2021-12-06 00:32:18 -0300

    Grammar now uses GrammarTransformer

--- gge/grammars.py
+++ gge/grammars.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from typing import Any, Iterable, Optional, Union
 
 import lark
+import typeguard
 
 _LarkTreeNode = Union[lark.Tree, str]
 
@@ -28,6 +29,7 @@ def _is_valid_name(value: str) -> bool:
     return all(chars_are_valid)
 
 
+@typeguard.typechecked
 @dataclass(order=True, frozen=True)
 class NonTerminal:
     text: str
@@ -54,7 +56,11 @@ class NonTerminal:
         if not core_is_valid:
             raise ValueError(error_msg)
 
+    def __repr__(self) -> str:
+        return f"NT({self.text})"
 
+
+@typeguard.typechecked
 @dataclass(order=True, frozen=True)
 class Terminal:
     text: str
@@ -85,10 +91,14 @@ class Terminal:
         if not core_is_valid:
             raise ValueError(error_msg)
 
+    def __repr__(self) -> str:
+        return f"T({self.text})"
+
 
 Symbol = Union[Terminal, NonTerminal]
 
 
+@typeguard.typechecked
 @dataclass(order=True, frozen=True)
 class RuleOption:
     """
@@ -106,11 +116,18 @@ class RuleOption:
     def __post_init__(self) -> None:
         assert len(self.symbols) >= 1
 
+    def __repr__(self) -> str:
+        return "RuleOption(" + ", ".join((repr(s) for s in self.symbols)) + ")"
 
+
+@typeguard.typechecked
 @dataclass(order=True, frozen=True)
 class ProductionRule:
     lhs: NonTerminal
     rhs: RuleOption
+
+    def __repr__(self) -> str:
+        return f"Rule({self.lhs}->{self.rhs})"
 
 
 class Grammar:
@@ -119,20 +136,25 @@ class Grammar:
         raw_grammar: str,
         raw_metagrammar: str,
     ) -> None:
-        n, t, p, s = extract_grammar_components(
-            raw_grammar,
-            raw_metagrammar,
-        )
-        validate_grammar_components(n, t, p, s)
+        parser = lark.Lark(raw_metagrammar, parser="lalr")
+        tree = parser.parse(raw_grammar)
+        transformer = GrammarTransformer()
+        transformer.transform(tree)
 
         self._raw_grammar = raw_grammar
 
-        self._nonterminals = n
-        self._terminals = t
-        self._rules = p
-        self._start_symbol = s
+        self._nonterminals = transformer.get_nonterminals()
+        self._terminals = transformer.get_terminals()
+        self._rules = transformer.get_rules()
+        self._start_symbol = transformer.get_start_symbol()
 
-        self._as_tuple = (n, t, p, s)
+        self._as_tuple = (
+            self._nonterminals,
+            self._terminals,
+            self._rules,
+            self._start_symbol,
+        )
+
         self._hash = hash(self._as_tuple)
 
     @property
@@ -178,328 +200,6 @@ class Grammar:
             return NotImplemented
 
         return self._as_tuple == other._as_tuple
-
-
-def _extract_lhs(rule: lark.Tree) -> NonTerminal:
-    assert rule.data == "rule"
-    lhs = rule.children[0]
-    assert isinstance(lhs, lark.Token)
-    return NonTerminal(lhs.value)
-
-
-def _conv_layer_marker(node: _LarkTreeNode) -> Terminal:
-    assert isinstance(node, lark.Token)
-    unquoted = node.value.replace('"', "")
-    return Terminal(unquoted)
-
-
-def _filter_counts(node: _LarkTreeNode) -> tuple[Terminal, list[Terminal]]:
-    assert isinstance(node, lark.Tree)
-    assert node.data == "filter_count"
-
-    marker_token, *count_nodes = node.children
-
-    assert isinstance(marker_token, lark.Token)
-    unquoted_marker = marker_token.value.replace('"', "")
-    marker = Terminal(unquoted_marker)
-
-    counts: list[Terminal] = []
-    for cn in count_nodes:
-        assert isinstance(cn, lark.Token)
-        cn_terminal = Terminal(cn.value)
-        counts.append(cn_terminal)
-
-    return marker, counts
-
-
-def _kernel_sizes(
-    node: _LarkTreeNode,
-) -> tuple[Terminal, list[Terminal]]:
-    assert isinstance(node, lark.Tree)
-    assert node.data == "kernel_size"
-
-    marker_node, *size_nodes = node.children
-
-    assert isinstance(marker_node, lark.Token)
-    unquoted = marker_node.value.replace('"', "")
-    marker = Terminal(unquoted)
-
-    sizes: list[Terminal] = []
-    for sn in size_nodes:
-        assert isinstance(sn, lark.Token)
-        sn_term = Terminal(sn.value)
-        sizes.append(sn_term)
-
-    return marker, sizes
-
-
-def _strides(node: _LarkTreeNode) -> tuple[Terminal, list[Terminal]]:
-    assert isinstance(node, lark.Tree)
-    assert node.data == "stride"
-
-    marker_node, *stride_nodes = node.children
-
-    assert isinstance(marker_node, lark.Token)
-    unquoted = marker_node.value.replace('"', "")
-    marker = Terminal(unquoted)
-
-    strides: list[Terminal] = []
-    for sn in stride_nodes:
-        assert isinstance(sn, lark.Token)
-        sn_term = Terminal(sn.value)
-        strides.append(sn_term)
-
-    return marker, strides
-
-
-def _rhs_from_conv_layer(node: lark.Tree) -> Iterable[RuleOption]:
-    assert node.data == "conv_layer"
-
-    (
-        conv_layer_marker_node,
-        filter_count_node,
-        kernel_size_node,
-        stride_node,
-    ) = node.children
-
-    conv_marker = _conv_layer_marker(conv_layer_marker_node)
-    filter_count_marker, filter_counts = _filter_counts(filter_count_node)
-    kernel_size_marker, kernel_sizes = _kernel_sizes(kernel_size_node)
-    stride_marker, strides = _strides(stride_node)
-
-    for fc, ks, st in itertools.product(filter_counts, kernel_sizes, strides):
-        symbols = (
-            conv_marker,
-            filter_count_marker,
-            fc,
-            kernel_size_marker,
-            ks,
-            stride_marker,
-            st,
-        )
-        yield RuleOption(symbols)
-
-
-def _rhs_from_dense_layer(node: lark.Tree) -> Iterable[RuleOption]:
-    assert node.data == "dense_layer"
-
-    marker_node, *units_tokens = node.children
-
-    assert isinstance(marker_node, lark.Token)
-    marker_terminal = Terminal(marker_node.value.replace('"', ""))
-
-    for ut in units_tokens:
-        assert isinstance(ut, lark.Token)
-        units_terminal = Terminal(ut.value)
-
-        symbols = (marker_terminal, units_terminal)
-        yield RuleOption(symbols)
-
-
-def _rhs_from_dropout_layer(node: lark.Tree) -> Iterable[RuleOption]:
-    assert node.data == "dropout_layer"
-
-    marker_token, *rate_tokens = node.children
-
-    assert isinstance(marker_token, lark.Token)
-    marker_terminal = Terminal(marker_token.value.replace('"', ""))
-
-    for rt in rate_tokens:
-        assert isinstance(rt, lark.Token)
-        rate_terminal = Terminal(rt.value)
-
-        symbols = (marker_terminal, rate_terminal)
-        yield RuleOption(symbols)
-
-
-def _rhss_from_layer(node: lark.Tree) -> Iterable[RuleOption]:
-    assert node.data == "layer"
-
-    assert len(node.children) == 1
-    child = node.children[0]
-    assert isinstance(child, lark.Tree)
-
-    if child.data == "conv_layer":
-        yield from _rhs_from_conv_layer(child)
-
-    elif child.data == "dense_layer":
-        yield from _rhs_from_dense_layer(child)
-
-    elif child.data == "dropout_layer":
-        yield from _rhs_from_dropout_layer(child)
-
-    else:
-        raise ValueError(f"unexpected node type: {child.data}")
-
-
-def _symbol_range(node: _LarkTreeNode) -> Iterable[list[NonTerminal]]:
-    assert isinstance(node, lark.Tree)
-    assert node.data == "symbol_range"
-
-    name_token, a, b = node.children
-
-    assert isinstance(name_token, lark.Token)
-    name = name_token.value
-    nt = NonTerminal(name)
-
-    assert a is None or isinstance(a, lark.Token)
-    assert b is None or isinstance(b, lark.Token)
-
-    if a is not None and b is not None:
-        start = int(a.value)
-        stop = int(b.value)
-    elif a is not None and b is None:
-        start = int(a.value)
-        stop = int(a.value)
-    elif a is None and b is None:
-        start = 1
-        stop = 1
-    else:
-        raise ValueError(f"unexpected symbol_range configuration: {node}")
-
-    assert start >= 0
-    assert stop >= start
-
-    for count in range(start, stop + 1):
-        yield [nt] * count
-
-
-def _rhss_from_block_option(node: _LarkTreeNode) -> Iterable[RuleOption]:
-    assert isinstance(node, lark.Tree)
-    assert node.data == "block_option"
-
-    repeated_symbols = [_symbol_range(sr) for sr in node.children]
-
-    for current_expansions in itertools.product(*repeated_symbols):
-        symbols = []
-        for ce in current_expansions:
-            symbols.extend(ce)
-
-        yield RuleOption(symbols=tuple(symbols))
-
-
-def _rhss_from_block(node: lark.Tree) -> Iterable[RuleOption]:
-    assert node.data == "block"
-
-    for option in node.children:
-        yield from _rhss_from_block_option(option)
-
-
-def _rhss_from_rule(rule: lark.Tree) -> Iterable[RuleOption]:
-    assert rule.data == "rule"
-
-    _, *options = rule.children
-
-    for opt in options:
-        assert isinstance(opt, lark.Tree)
-
-        if opt.data == "layer":
-            yield from _rhss_from_layer(opt)
-
-        elif opt.data == "block":
-            yield from _rhss_from_block(opt)
-
-        else:
-            raise ValueError(f"unknown node type {opt.data}")
-
-
-def extract_rules(tree: lark.Tree) -> Iterable[ProductionRule]:
-    assert tree.data == "start"
-
-    for rule in tree.children:
-        assert isinstance(rule, lark.Tree)
-
-        lhs = _extract_lhs(rule)
-
-        for rhs in _rhss_from_rule(rule):
-            yield ProductionRule(lhs, rhs)
-
-
-def extract_nonterminals(tree: lark.Tree) -> set[NonTerminal]:
-    assert tree.data == "start"
-
-    nonterminals: set[NonTerminal] = set()
-
-    for rule in tree.children:
-        assert isinstance(rule, lark.Tree)
-        assert rule.data == "rule"
-
-        lhs = rule.children[0]
-        assert isinstance(lhs, lark.Token)
-
-        nt = NonTerminal(text=lhs.value)
-        nonterminals.add(nt)
-
-    return nonterminals
-
-
-_TERMINAL_TOKEN_TYPES = {
-    "CONV2D",
-    "FILTER_COUNT",
-    "KERNEL_SIZE",
-    "STRIDE",
-    "DENSE",
-    "DROPOUT",
-    "INT",
-    "FLOAT",
-}
-
-
-def _is_terminal_token(node: _LarkTreeNode) -> bool:
-    if not isinstance(node, lark.Token):
-        return False
-
-    return node.type in _TERMINAL_TOKEN_TYPES
-
-
-def extract_terminals(
-    tree: lark.Tree,
-    nonterminals: set[NonTerminal],
-) -> set[Terminal]:
-    assert tree.data == "start"
-
-    terminals: set[Terminal] = set()
-    for node in tree.scan_values(pred=_is_terminal_token):
-        token_text = node.replace('"', "")
-        terminals.add(Terminal(token_text))
-
-    # sanity check
-    nonterminals_texts = set(nt.text for nt in nonterminals)
-    for term in terminals:
-        if term.text in nonterminals_texts:
-            raise ValueError(f"text is both a terminal and a nonterminal: {term.text}")
-
-    return terminals
-
-
-def extract_grammar_components(
-    raw_grammar: str,
-    raw_metagrammar: str,
-) -> tuple[
-    tuple[NonTerminal, ...],
-    tuple[Terminal, ...],
-    tuple[ProductionRule, ...],
-    NonTerminal,
-]:
-    """
-    Returns the standard components of a grammar = (N, T, P, S)
-    N = Nonterminals
-    T = Terminals
-    P = Production rules
-    S = Start symbol
-    """
-    assert raw_grammar
-    assert raw_metagrammar
-
-    parser = lark.Lark(raw_metagrammar)
-    tree = parser.parse(raw_grammar)
-
-    rules = extract_rules(tree)
-    nonterminals = extract_nonterminals(tree)
-    terminals = extract_terminals(tree, nonterminals)
-    start = NonTerminal("start")
-
-    return tuple(nonterminals), tuple(terminals), tuple(rules), start
 
 
 def _all_nonterminals_are_defined(
@@ -686,39 +386,54 @@ class GrammarTransformer(lark.Transformer[list[ProductionRule]]):
 
         return nonterm
 
-    def start(self, rules: list[ProductionRule]) -> list[ProductionRule]:
+    def start(self, list_of_list_of_rules: Any) -> list[ProductionRule]:
         assert not self._frozen
         assert self._start is None
 
         self._start = NonTerminal("start")
-        self._rules = rules
 
-        return rules
+        flattened = [
+            rule for list_of_rules in list_of_list_of_rules for rule in list_of_rules
+        ]
+        self._rules = flattened
 
-    def rule(
-        self,
-        rule_parts: tuple[NonTerminal, list[RuleOption]],
-    ) -> list[ProductionRule]:
+        return flattened
+
+    def rule(self, rule_parts: Any) -> list[ProductionRule]:
         assert not self._frozen
 
-        lhs, options = rule_parts
-        return [ProductionRule(lhs=lhs, rhs=opt) for opt in options]
+        lhs, *list_of_list_of_options = rule_parts
 
-    def block(self, block_options: list[RuleOption]) -> list[RuleOption]:
-        return block_options
+        return [
+            ProductionRule(lhs, rhs)
+            for list_of_options in list_of_list_of_options
+            for rhs in list_of_options
+        ]
+
+    def block(self, list_of_list_of_options: Any) -> list[RuleOption]:
+        assert not self._frozen
+
+        return [
+            option
+            for list_of_options in list_of_list_of_options
+            for option in list_of_options
+        ]
 
     def block_option(
         self,
         repeated_symbols: list[Iterable[Symbol]],
     ) -> list[RuleOption]:
-        return [
-            RuleOption(tuple(symbols))
-            for symbols in itertools.product(*repeated_symbols)
-        ]
+        assert not self._frozen
+
+        combinations = itertools.product(*repeated_symbols)
+        unpacked = [itertools.chain(*inner) for inner in combinations]
+        return [RuleOption(tuple(symbols)) for symbols in unpacked]
 
     def symbol_range(
         self, parts: tuple[NonTerminal, Optional[int], Optional[int]]
     ) -> list[list[NonTerminal]]:
+        assert not self._frozen
+
         name, a, b = parts
 
         if a is not None and b is not None:
@@ -741,8 +456,14 @@ class GrammarTransformer(lark.Transformer[list[ProductionRule]]):
 
         return [[name] * count for count in range(start, stop + 1)]
 
-    def layer(self, options: list[RuleOption]) -> list[RuleOption]:
-        return options
+    def layer(self, list_of_lists_of_options: Any) -> list[RuleOption]:
+        assert not self._frozen
+
+        return [
+            opt
+            for list_of_options in list_of_lists_of_options
+            for opt in list_of_options
+        ]
 
     def conv_layer(self, parts: Any) -> list[RuleOption]:
         assert not self._frozen
@@ -825,7 +546,6 @@ class GrammarTransformer(lark.Transformer[list[ProductionRule]]):
 def main() -> None:
     data_dir = Path(__file__).parent.parent / "data"
     metagrammar = (data_dir / "metagrammar.lark").read_text()
-    # grammar = (data_dir / "simple_grammar.lark").read_text()
     grammar = r"""
         start: simple_block complex_block simple_dense
 
@@ -852,9 +572,6 @@ def main() -> None:
 
     for r in rules:
         print(r)
-
-    for t in extractor.get_terminals():
-        print(t)
 
 
 if __name__ == "__main__":
