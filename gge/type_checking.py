01c175506ee53a0217ab01b9935b3909453fa9d5
Mirandatz <Mirandatz@users.noreply.github.com>
2021-12-30 00:03:29 -0300

    Update type_checking.py api

--- gge/type_checking.py
+++ gge/type_checking.py
@@ -3,11 +3,10 @@ import typing
 _T = typing.TypeVar("_T")
 
 
-def assert_tuple_is_homogeneous(tup: tuple[_T, ...]) -> None:
+def assert_tuple_is_homogeneous(tup: tuple[_T, ...], expected_type: type) -> None:
     assert isinstance(tup, tuple)
     if len(tup) == 0:
         return
 
-    expected_type = type(tup[0])
-    for item in tup[1:]:
+    for item in tup:
         assert isinstance(item, expected_type)
