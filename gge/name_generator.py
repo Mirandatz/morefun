c051f64aa14725fc106706c973f5d4e09ed4c9d5
Mirandatz <Mirandatz@users.noreply.github.com>
2022-01-01 18:27:58 -0300

    Add new functionality to NameGenerator

--- gge/name_generator.py
+++ gge/name_generator.py
@@ -5,10 +5,20 @@ class NameGenerator:
     def __init__(self) -> None:
         self._instance_counter: collections.Counter[str] = collections.Counter()
 
-    def create_name(self, prefix: str) -> str:
+    def _create_name(self, prefix: str) -> str:
         assert prefix
 
         count = self._instance_counter[prefix]
         name = f"{prefix}_{count}"
         self._instance_counter[prefix] += 1
         return name
+
+    def create_name(self, prefix_or_type: str | type) -> str:
+        if isinstance(prefix_or_type, str):
+            return self._create_name(prefix_or_type)
+
+        elif isinstance(prefix_or_type, type):
+            return self._create_name(prefix_or_type.__name__)
+
+        else:
+            raise ValueError("prefix_or_type must be a str or a type")
--- gge/tests/test_name_generator.py
+++ gge/tests/test_name_generator.py
@@ -1,8 +1,9 @@
-import gge.name_generator as gge_namegen
+import gge.layers as gl
+import gge.name_generator as ng
 
 
 def test_first_prefix() -> None:
-    gen = gge_namegen.NameGenerator()
+    gen = ng.NameGenerator()
     first = gen.create_name("k")
     assert "k_0" == first
 
@@ -11,8 +12,19 @@ def test_first_prefix() -> None:
 
 
 def test_multiple_prefixes() -> None:
-    gen = gge_namegen.NameGenerator()
+    gen = ng.NameGenerator()
     _ = gen.create_name("x")
 
     actual = gen.create_name("y")
     assert "y_0" == actual
+
+
+def test_type() -> None:
+    gen = ng.NameGenerator()
+    actual_first = gen.create_name(gl.Conv2D)
+    expected_first = "Conv2D_0"
+    assert expected_first == actual_first
+
+    actual_second = gen.create_name(gl.Conv2D)
+    expected_second = "Conv2D_1"
+    assert expected_second == actual_second
