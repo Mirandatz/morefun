4dc6b3f62f67a5909a621638826f2ed1c045c4ff
Mirandatz <Mirandatz@users.noreply.github.com>
2022-01-09 12:00:11 -0300

    Reorganize layer tests

--- gge/tests/test_layers.py
+++ gge/tests/test_layers.py
@@ -19,3 +19,25 @@ def test_different_aspect_ratio() -> None:
     b = gl.Shape(1, 3, 3)
 
     assert a.aspect_ratio != b.aspect_ratio
+
+
+def test_conv2d_output_depth_non_unity_stride() -> None:
+    connected = gl.ConnectedConv2D(
+        gl.Input(gl.Shape(1024, 1024, 3)),
+        gl.Conv2D(name="a", filter_count=17, kernel_size=5, stride=4),
+    )
+
+    actual = connected.output_shape
+    expected = gl.Shape(256, 256, 17)
+    assert expected == actual
+
+
+def test_conv2d_output_depth_unity_stride() -> None:
+    connected = gl.ConnectedConv2D(
+        gl.Input(gl.Shape(1024, 1024, 3)),
+        gl.Conv2D(name="a", filter_count=17, kernel_size=5, stride=1),
+    )
+
+    actual = connected.output_shape
+    expected = gl.Shape(1024, 1024, 17)
+    assert expected == actual
