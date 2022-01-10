f41988e520f2c7a2601128e2648abd8d0db5f35e
Mirandatz <Mirandatz@users.noreply.github.com>
2022-01-09 23:18:41 -0300

    Update gge/tests/test_layers.py
    Co-authored-by: Diorge Brognara <79663752+diorge@users.noreply.github.com>

--- gge/tests/test_layers.py
+++ gge/tests/test_layers.py
@@ -32,7 +32,7 @@ def test_conv2d_output_depth_non_unit_stride() -> None:
     assert expected == actual
 
 
-def test_conv2d_output_depth_unity_stride() -> None:
+def test_conv2d_output_depth_unit_stride() -> None:
     connected = gl.ConnectedConv2D(
         gl.Input(gl.Shape(1024, 1024, 3)),
         gl.Conv2D(name="a", filter_count=17, kernel_size=5, stride=1),
