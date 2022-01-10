732895727fcb7a4af66d876c170bcf1cebe1024b
Mirandatz <Mirandatz@users.noreply.github.com>
2022-01-09 23:18:34 -0300

    Update gge/tests/test_layers.py
    Co-authored-by: Diorge Brognara <79663752+diorge@users.noreply.github.com>

--- gge/tests/test_layers.py
+++ gge/tests/test_layers.py
@@ -21,7 +21,7 @@ def test_different_aspect_ratio() -> None:
     assert a.aspect_ratio != b.aspect_ratio
 
 
-def test_conv2d_output_depth_non_unity_stride() -> None:
+def test_conv2d_output_depth_non_unit_stride() -> None:
     connected = gl.ConnectedConv2D(
         gl.Input(gl.Shape(1024, 1024, 3)),
         gl.Conv2D(name="a", filter_count=17, kernel_size=5, stride=4),
