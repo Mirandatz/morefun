f6610da17634ab42d8473c69c42f788d7dbc6f31
Diorge Brognara <diorge.bs@gmail.com>
2022-01-12 09:50:52 -0300

    Changing test dataclass to property
    Shouldn't matter but looks nicer

--- gge/tests/strategies.py
+++ gge/tests/strategies.py
@@ -9,11 +9,13 @@ class GrammarOption:
     """the mesagrammar '(1 | 2 | 3)' string has values [1, 2, 3]"""
 
     possible_values: list[int]
-    mesagrammar_string: str
+
+    @property
+    def mesagrammar_string(self) -> str:
+        return f"({'|'.join(map(str, self.possible_values))})"
 
 
 @hs.composite
 def grammar_integer_option(draw: typing.Callable[..., typing.Any]) -> GrammarOption:
     ints = draw(hs.lists(hs.integers(min_value=1), min_size=2, unique=True))
-    composite_string = f"({'|'.join(map(str, ints))})"
-    return GrammarOption(ints, composite_string)
+    return GrammarOption(ints)
