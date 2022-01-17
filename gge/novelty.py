87febca248469587f3d0550b68b3ee91531b05a4
Mirandatz <Mirandatz@users.noreply.github.com>
2022-01-17 13:55:27 -0300

    Mark NoveltyTracker for refactoring

--- gge/novelty.py
+++ gge/novelty.py
@@ -4,23 +4,16 @@ import gge.neural_network as gnn
 
 class NoveltyTracker:
     def __init__(self) -> None:
-        self._known_genotypes: set[cg.CompositeGenotype] = set()
-        self._known_phenotypes: set[gnn.NeuralNetwork] = set()
+        raise NotImplementedError()
 
     def is_genotype_novel(self, genotype: cg.CompositeGenotype) -> bool:
-        return genotype in self._known_genotypes
+        raise NotImplementedError()
 
     def is_phenotype_novel(self, phenotype: gnn.NeuralNetwork) -> bool:
-        return phenotype in self._known_phenotypes
+        raise NotImplementedError()
 
     def register_known_genotype(self, genotype: cg.CompositeGenotype) -> None:
-        if genotype in self._known_genotypes:
-            raise ValueError("genotype is already known")
-
-        self._known_genotypes.add(genotype)
+        raise NotImplementedError()
 
     def register_phenotype(self, phenotype: gnn.NeuralNetwork) -> None:
-        if phenotype in self._known_phenotypes:
-            raise ValueError("phenotype is already known")
-
-        self._known_phenotypes.add(phenotype)
+        raise NotImplementedError()
