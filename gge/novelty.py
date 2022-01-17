2e93318c425bf5598c0543c69da854a3eb00ad5a
Mirandatz <Mirandatz@users.noreply.github.com>
2022-01-17 14:28:48 -0300

    Reimplement NoveltyTracker

--- gge/novelty.py
+++ gge/novelty.py
@@ -3,23 +3,32 @@ import gge.neural_network as gnn
 
 
 class NoveltyTracker:
-    def __init__(self) -> None:
-        raise NotImplementedError()
+    def __init__(
+        self,
+        known_genotypes: set[cg.CompositeGenotype] = set(),
+        known_phenotypes: set[gnn.NeuralNetwork] = set(),
+    ) -> None:
+        self._genotypes = known_genotypes.copy()
+        self._phenotypes = known_phenotypes.copy()
 
     def copy(self) -> "NoveltyTracker":
-        raise NotImplementedError()
+        return NoveltyTracker(
+            known_genotypes=self._genotypes,
+            known_phenotypes=self._phenotypes,
+        )
 
     def update(self, other: "NoveltyTracker") -> None:
-        raise NotImplementedError()
+        self._genotypes.update(other._genotypes)
+        self._phenotypes.update(other._phenotypes)
 
     def is_genotype_novel(self, genotype: cg.CompositeGenotype) -> bool:
-        raise NotImplementedError()
+        return genotype in self._genotypes
 
     def is_phenotype_novel(self, phenotype: gnn.NeuralNetwork) -> bool:
-        raise NotImplementedError()
+        return phenotype in self._phenotypes
 
     def register_genotype(self, genotype: cg.CompositeGenotype) -> None:
-        raise NotImplementedError()
+        self._genotypes.add(genotype)
 
     def register_phenotype(self, phenotype: gnn.NeuralNetwork) -> None:
-        raise NotImplementedError()
+        self._phenotypes.add(phenotype)
