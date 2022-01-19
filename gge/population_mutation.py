ab41c7c64ddbeb86ceb4c0b41287f9f631478adc
Diorge Brognara <diorge.bs@gmail.com>
2022-01-19 10:39:55 -0300

    Adding logging to population_mutation

--- gge/population_mutation.py
+++ gge/population_mutation.py
@@ -1,5 +1,7 @@
 import functools
 
+from loguru import logger
+
 import gge.composite_genotypes as cg
 import gge.fallible as fallible
 import gge.grammars as gr
@@ -18,19 +20,24 @@ def try_generate_mutant(
     rng: rand.RNG,
     novelty_tracker: novel.NoveltyTracker,
 ) -> cg.CompositeGenotype | None:
+    logger.trace("try_generate_mutant")
 
     candidate: cg.CompositeGenotype = rng.choice(population)  # type: ignore
 
     mutant = mutations.mutate(candidate, grammar, rng)
     if novelty_tracker.is_genotype_novel(mutant):
+        logger.debug(f"New novelty mutant=<{mutant}>")
         novelty_tracker.register_genotype(mutant)
     else:
+        logger.debug("Failed to produce a novelty individual")
         return None
 
     phenotype = gnn.make_network(mutant, grammar, DUMMY_INPUT)
     if novelty_tracker.is_phenotype_novel(phenotype):
+        logger.debug(f"New novelty phenotype=<{phenotype}>")
         novelty_tracker.register_phenotype(phenotype)
     else:
+        logger.debug("Failed to produce a novelty phenotype")
         return None
 
     return mutant
