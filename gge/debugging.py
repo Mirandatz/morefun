367febc638c81ff6bf5998a1650c60dc7ae8e424
Mirandatz <Mirandatz@users.noreply.github.com>
2022-01-21 17:59:55 -0300

    Add more debugging methods

--- gge/debugging.py
+++ gge/debugging.py
@@ -3,6 +3,9 @@ import pickle
 import tempfile
 import typing
 
+import keras
+import tensorflow as tf
+
 import gge.composite_genotypes as cg
 
 DEBUG_DATA_DIR = pathlib.Path(__file__).parent.parent / "debug_data"
@@ -11,7 +14,11 @@ DEBUG_DATA_DIR.mkdir(exist_ok=True)
 
 def save_genotype(genotype: cg.CompositeGenotype) -> str:
     serialized = pickle.dumps(genotype, protocol=pickle.HIGHEST_PROTOCOL)
-    with tempfile.NamedTemporaryFile(dir=DEBUG_DATA_DIR, delete=False) as file:
+    with tempfile.NamedTemporaryFile(
+        dir=DEBUG_DATA_DIR,
+        delete=False,
+        suffix=".genotype",
+    ) as file:
         file.write(serialized)
         return file.name
 
@@ -20,3 +27,18 @@ def load_genotype(filename: str) -> cg.CompositeGenotype:
     serialized = (DEBUG_DATA_DIR / filename).read_bytes()
     deserialized = pickle.loads(serialized)
     return typing.cast(cg.CompositeGenotype, deserialized)
+
+
+def plot_tf_model(model: keras.Model) -> str:
+    with tempfile.NamedTemporaryFile(
+        dir=DEBUG_DATA_DIR,
+        delete=False,
+        suffix=".png",
+    ) as file:
+        tf.keras.utils.plot_model(
+            model,
+            file.name,
+            show_shapes=True,
+            show_layer_names=False,
+        )
+        return file.name
