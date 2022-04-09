365462ea78a90331343adc11096924687f0dd519
Mirandatz <Mirandatz@users.noreply.github.com>
2022-04-09 18:39:36 -0300

    Add 'rng seed' to cifar10 template scripts

--- gge/experiments/cifar10/run_container.bash
+++ gge/experiments/cifar10/run_container.bash
@@ -1,5 +1,11 @@
 #!/bin/bash -x
 
+if [[ -z $RNG_SEED ]]; then
+    echo "RNG_SEED not defined"
+    exit -1
+fi
+
+# customize for different machines
 GGE_SRC="${GGE_SRC:=/home/thiago/source/gge}"
 DATASETS_DIR="${DATASETS_DIR:=/home/thiago/source/datasets/cifar10}"
 OUTPUT_DIR="${OUTPUT_DIR:=/dev/shm/hehe}"
@@ -11,6 +17,7 @@ docker run \
 	--runtime=nvidia \
 	--shm-size=8G \
 	--workdir=/src \
+	--env RNG_SEED=$RNG_SEED \
 	-v "$GGE_SRC":/src:ro \
 	-v "$DATASETS_DIR":/dataset:ro \
 	-v "$OUTPUT_DIR":/output \
--- gge/experiments/cifar10/run_gge.bash
+++ gge/experiments/cifar10/run_gge.bash
@@ -1,9 +1,15 @@
 #!/bin/bash -x
 
-# assumes containerized environment, configured by run_container.bash
+# this script assumes its being run in a containerized environment configured by run_container.bash
+
+if [[ -z $RNG_SEED ]]; then
+    echo "RNG_SEED not defined"
+    exit -1
+fi
 
 tar -xf /dataset/train_val_test.tar.gz -C /dev/shm/
 
 python -m gge.experiments.cifar10.run_evolution \
     -d /dev/shm/train_val_test \
     -o /output
+    -s $RNG_SEED
