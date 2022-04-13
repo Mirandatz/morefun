e881af6c3247ab43e7b9e8d9166091868cf8b16a
Mirandatz <Mirandatz@users.noreply.github.com>
2022-04-12 21:00:58 -0300

    Simplify experiment scripts

--- gge/experiments/cifar10/run_container.bash
+++ gge/experiments/cifar10/run_container.bash
@@ -1,18 +1,21 @@
 #!/bin/bash -eux
 
-DOCKER_ENV_TAG=mirandatz/gge:dev_env
-
 SCRIPT_DIR=$(dirname $(realpath "$0"))
 GGE_SRC=$(realpath "${SCRIPT_DIR}/../../..")
 
 docker run \
+	--user $(id -u):$(id -g) \
 	--rm \
 	--runtime=nvidia \
 	--shm-size=8G \
 	--workdir=/src \
 	--env RNG_SEED=$RNG_SEED \
 	-v "$GGE_SRC":/src:ro \
-	-v "$DATASET_DIR":/dataset:ro \
+	-v "$DATASETS_DIR/cifar10":/dataset:ro \
 	-v "$OUTPUT_DIR":/output \
-	"$DOCKER_ENV_TAG" \
-	/src/gge/experiments/cifar10/run_gge.bash
+	mirandatz/gge:dev_env \
+	python -m gge.experiments.cifar10.evolution \
+		--train /dataset/train \
+		--validation /dataset/val \
+		--output /output \
+		--seed $RNG_SEED
