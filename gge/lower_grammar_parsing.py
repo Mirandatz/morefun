fea5f0fc22d9071b4c3ae4062c9cc5a1af6208cb
Mirandatz <Mirandatz@users.noreply.github.com>
2022-04-21 00:31:27 -0300

    Improve lower_grammar_parsing.py api

--- gge/lower_grammar_parsing.py
+++ gge/lower_grammar_parsing.py
@@ -11,18 +11,36 @@ LOWER_GRAMMAR_PATH = (
 )
 
 
-def get_parser() -> lark.Lark:
-    logger.debug("starting parsing lower grammar")
-    parser = lark.Lark.open(grammar_filename=str(LOWER_GRAMMAR_PATH), parser="lalr")
-    logger.debug("finished parsing lower grammar")
+def get_parser(start: str) -> lark.Lark:
+    logger.trace("starting parsing lower grammar")
+    parser = lark.Lark.open(
+        grammar_filename=str(LOWER_GRAMMAR_PATH),
+        start=start,
+        parser="lalr",
+    )
+    logger.trace("finished parsing lower grammar")
     return parser
 
 
-def parse_lower_grammar_tokenstream(tokenstream: str) -> lark.Tree[typing.Any]:
-    logger.debug("starting parsing lower grammar tokenstream")
-    tree = get_parser().parse(tokenstream)
-    logger.debug("finished parsing lower grammar tokenstream")
-    return tree
+def parse_tokenstream(
+    tokenstream: str,
+    start: str,
+    relevant_subtree: str,
+) -> lark.Tree[typing.Any]:
+    """
+    Parses `tokenstream` using the lower_grammar,
+    assuming the "start symbol" of the grammar is `start` generating
+    a tree.
+    Returns the subtree associated of the `relevant_subtree` node.
+    """
+
+    logger.trace("starting parsing lower grammar tokenstream")
+    tree = get_parser(start).parse(tokenstream)
+    subtrees = list(tree.find_data(relevant_subtree))
+    assert len(subtrees) == 1
+
+    logger.trace("finished parsing lower grammar tokenstream")
+    return subtrees[0]
 
 
 @lark.v_args(inline=True)
