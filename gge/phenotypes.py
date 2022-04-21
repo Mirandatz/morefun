8136579962e60dcb662ae1c47d17060d1ccd1c05
Mirandatz <Mirandatz@users.noreply.github.com>
2022-04-21 19:16:23 -0300

    Add new function to phenotypes.py

--- gge/phenotypes.py
+++ gge/phenotypes.py
@@ -1,12 +1,14 @@
 import uuid
 
 import attrs
+import tensorflow as tf
 
 import gge.backbones as bb
 import gge.composite_genotypes as cg
 import gge.connections as conn
 import gge.data_augmentations as gda
 import gge.grammars as gr
+import gge.layers as gl
 import gge.optimizers as optim
 import gge.structured_grammatical_evolution as sge
 
@@ -40,3 +42,17 @@ def translate(
         optimizer,
         genotype.unique_id,
     )
+
+
+def make_input_output_tensors(
+    phenotype: Phenotype,
+    input_layer: gl.Input,
+) -> tuple[tf.Tensor, tf.Tensor]:
+    output_layer = conn.connect_backbone(
+        phenotype.backbone,
+        phenotype.connections,
+        input_layer,
+    )
+    known_tensors: dict[gl.ConnectableLayer, tf.Tensor] = {}
+    output_layer.to_tensor(known_tensors)
+    return known_tensors[input_layer], known_tensors[output_layer]
