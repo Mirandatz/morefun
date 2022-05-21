66c81fb8a7c7de61328c7aa9b6a1a0d53e28e206
Mirandatz <Mirandatz@users.noreply.github.com>
2022-05-21 15:09:06 -0300

    Add convenience function to novelty.py

--- gge/novelty.py
+++ gge/novelty.py
@@ -1,12 +1,12 @@
 import gge.composite_genotypes as cg
-import gge.phenotypes as cph
+import gge.phenotypes as phenos
 
 
 class NoveltyTracker:
     def __init__(
         self,
         known_genotypes: set[cg.CompositeGenotype] | None = None,
-        known_phenotypes: set[cph.Phenotype] | None = None,
+        known_phenotypes: set[phenos.Phenotype] | None = None,
     ) -> None:
         self._genotypes = (known_genotypes or set()).copy()
         self._phenotypes = (known_phenotypes or set()).copy()
@@ -24,11 +24,33 @@ class NoveltyTracker:
     def is_genotype_novel(self, genotype: cg.CompositeGenotype) -> bool:
         return genotype not in self._genotypes
 
-    def is_phenotype_novel(self, phenotype: cph.Phenotype) -> bool:
+    def is_phenotype_novel(self, phenotype: phenos.Phenotype) -> bool:
         return phenotype not in self._phenotypes
 
     def register_genotype(self, genotype: cg.CompositeGenotype) -> None:
         self._genotypes.add(genotype)
 
-    def register_phenotype(self, phenotype: cph.Phenotype) -> None:
+    def register_phenotype(self, phenotype: phenos.Phenotype) -> None:
         self._phenotypes.add(phenotype)
+
+
+def check_novelty_and_update_tracker(
+    genotype: cg.CompositeGenotype,
+    phenotype: phenos.Phenotype,
+    tracker: NoveltyTracker,
+) -> bool:
+    if not tracker.is_genotype_novel(genotype):
+        return False
+
+    if not tracker.is_phenotype_novel(phenotype):
+        # got a new genotype that maps to a known phenotype,
+        # so we mark the genotype as known and return false
+        tracker.register_genotype(genotype)
+        return False
+
+    else:
+        # found a new genotype that maps to new phenotype,
+        # so we mark both as known and return true
+        tracker.register_genotype(genotype)
+        tracker.register_phenotype(phenotype)
+        return True
---
+++ gge/tests/test_novelty_tracking.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+@pytest.mark.xfail
+def test_convenience_function() -> None:
+    raise NotImplementedError()
