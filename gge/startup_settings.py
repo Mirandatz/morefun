cbe943801f7925497a5f678b8b73faad3510d990
MirandaTZ <thiago.zafalon.miranda@gmail.com>
2022-07-20 16:17:50 -0300

    Add more functions to startup_settings.py and fix typos

--- gge/startup_settings.py
+++ gge/startup_settings.py
@@ -13,6 +13,8 @@ import sys
 import typer
 from loguru import logger
 
+import gge.redirection
+
 # TODO: improve validation and error messages
 
 GGE_DIR = pathlib.Path("/gge")
@@ -37,7 +39,7 @@ GRAMMAR_PATH = typer.Option(
 INITIAL_POPULATION_DIR = typer.Option(
     GGE_DIR / "initial_population",
     "--initial-population-dir",
-    envvar="GGE_INITIAL_POPULAITON_DIR",
+    envvar="GGE_INITIAL_POPULATION_DIR",
     dir_okay=True,
     exists=True,
     readable=True,
@@ -131,10 +133,10 @@ EPOCHS = typer.Option(default=..., envvar="GGE_EPOCHS", min=1)
 
 def configure_logger(log_dir: pathlib.Path, log_level: str) -> None:
     if log_level not in ["DEBUG", "INFO", "WARNING"]:
-        raise ValueError("uknown log_level=<{log_level}>")
+        raise ValueError(f"unknown log_level=<{log_level}>")
 
     if not log_dir.is_dir():
-        raise ValueError("log_dir is not dir, path=<{log_dir}>")
+        raise ValueError(f"log_dir is not a directory, path=<{log_dir}>")
 
     logger.remove()
     logger.add(sink=sys.stderr, level=log_level)
@@ -142,3 +144,36 @@ def configure_logger(log_dir: pathlib.Path, log_level: str) -> None:
 
     # checking if we can write to both sinks
     logger.info("started logger")
+
+
+def validate_dataset_dir(
+    path: pathlib.Path,
+    img_height: int,
+    img_width: int,
+    expected_num_instances: int,
+    expected_class_count: int,
+) -> None:
+    import tensorflow as tf
+
+    with gge.redirection.discard_stderr_and_stdout():
+        with tf.device("cpu"):
+            ds: tf.data.Dataset = tf.keras.utils.image_dataset_from_directory(
+                directory=path,
+                batch_size=None,
+                image_size=(img_height, img_width),
+                label_mode="categorical",
+                shuffle=False,
+                color_mode="rgb",
+            )
+
+            num_instances = ds.cardinality().numpy()
+            if num_instances != expected_num_instances:
+                raise ValueError(
+                    f"unexpected number of instances. found=<{num_instances}>, expected=<{expected_num_instances}>"
+                )
+
+            num_classes = len(ds.class_names)
+            if num_classes != expected_class_count:
+                raise ValueError(
+                    f"unexpected number of classes. found=<{num_classes}>, expected=<{expected_class_count}>"
+                )
