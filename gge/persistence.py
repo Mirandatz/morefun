819ab9c3da57e2b13079feb3497fcf4ce55e07bc
MirandaTZ <thiago.zafalon.miranda@gmail.com>
2022-08-17 17:08:09 -0300

    Add more functions to persistency.py

--- gge/persistence.py
+++ gge/persistence.py
@@ -14,32 +14,50 @@ def save_genotype_to_directory(
 ) -> None:
     assert directory.is_dir()
 
-    filename = directory / f"{genotype.unique_id.hex}{GENOTYPE_EXTENSION}"
     serialized = serialize_genotype(genotype)
-    filename.write_bytes(serialized)
+
+    path_without_extension = directory / genotype.unique_id.hex
+    full_path = path_without_extension.with_suffix(GENOTYPE_EXTENSION)
+
+    full_path.write_bytes(serialized)
 
 
 def save_population_genotypes(
     population: list[cg.CompositeGenotype],
     directory: pathlib.Path,
 ) -> None:
+    assert len(population) > 0
     assert directory.is_dir()
 
     logger.info("started saving population genotypes")
 
-    saved = 0
-
     for genotype in population:
         save_genotype_to_directory(genotype, directory)
-        saved += 1
 
-    if saved == 0:
-        logger.warning("`population` was empty, no genotypes were saved")
+    logger.info("finished saving population genotypes")
 
-    logger.info(
-        f"finished saving population genotypes, number of saved genotypes=<{saved}>"
-    )
+
+def load_genotype(path: pathlib.Path) -> cg.CompositeGenotype:
+    return deserialize_genotype(path.read_bytes())
+
+
+def load_population_genotypes(directory: pathlib.Path) -> list[cg.CompositeGenotype]:
+    assert directory.is_dir()
+
+    paths = list(directory.glob(f"*{GENOTYPE_EXTENSION}"))
+    if len(paths) == 0:
+        raise ValueError(
+            f"directory=<{directory}> does not contain files with genotype extension=<{GENOTYPE_EXTENSION}>"
+        )
+
+    return [load_genotype(p) for p in paths]
 
 
 def serialize_genotype(genotype: cg.CompositeGenotype) -> bytes:
     return pickle.dumps(genotype, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def deserialize_genotype(serialized: bytes) -> cg.CompositeGenotype:
+    genotype = pickle.loads(serialized)
+    assert isinstance(genotype, cg.CompositeGenotype)
+    return genotype
