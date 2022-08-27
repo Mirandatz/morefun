6862c0a1c75dfa883099ffc536da8d010af7935b
MirandaTZ <thiago.zafalon.miranda@gmail.com>
2022-08-27 15:53:57 -0300

    Add new persistence function

--- gge/persistence.py
+++ gge/persistence.py
@@ -1,7 +1,10 @@
 import pathlib
 import pickle
+import shutil
+import tempfile
 
 import attrs
+import tensorflow as tf
 from loguru import logger
 
 import gge.fitnesses as cf
@@ -68,3 +71,43 @@ def load_latest_generation_output(output_dir: pathlib.Path) -> GenerationOutput:
     paths = output_dir.glob(f"*{GENERATION_OUTPUT_EXTENSION}")
     latest = max(paths, key=lambda path: int(path.stem))
     return load_generation_output(latest)
+
+
+def save_and_zip_tf_model(model: tf.keras.Model, path: pathlib.Path) -> None:
+    """
+    Saves a Tensorflow model to a single file.
+
+    Obs: The difference between this function and the one provided by Tensorflow
+    is that Tensorflow's `save` function creates a directory with multiple files.
+    """
+
+    with tempfile.TemporaryDirectory(dir="/dev/shm") as _dir:
+        tmp_dir = pathlib.Path(_dir)
+
+        model_dir = tmp_dir / "tf_model"
+
+        tf.keras.models.save_model(
+            model=model,
+            filepath=model_dir,
+            include_optimizer=True,
+            save_format="tf",
+            save_traces=True,
+        )
+
+        zipped = shutil.make_archive(
+            base_name=str(tmp_dir / "tf_model_zipped"),
+            format="zip",
+            root_dir=model_dir,
+            base_dir=".",
+        )
+
+        shutil.move(src=zipped, dst=path)
+
+
+def load_zipped_tf_model(path: pathlib.Path) -> tf.keras.Model:
+    with tempfile.TemporaryDirectory(dir="/dev/shm") as _dir:
+        tmp_dir = pathlib.Path(_dir)
+
+        shutil.unpack_archive(filename=path, extract_dir=tmp_dir, format="zip")
+
+        return tf.keras.models.load_model(tmp_dir)
