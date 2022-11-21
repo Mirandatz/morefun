4fc14ebb7b7582dfb1f828fc6629ed0406cd645e
MirandaTZ <thiago.zafalon.miranda@gmail.com>
2022-11-21 19:51:49 -0300

    Move path-related methods from persistence.py to paths.py

--- gge/persistence.py
+++ gge/persistence.py
@@ -6,9 +6,8 @@ from loguru import logger
 import gge.composite_genotypes as cg
 import gge.evolutionary.fitnesses as gf
 import gge.evolutionary.novelty
+import gge.paths
 import gge.randomness as rand
-
-GENERATION_OUTPUT_EXTENSION = ".gen_out2"
 
 
 class GenerationOutput:
@@ -39,13 +38,6 @@ class GenerationOutput:
         return rng
 
 
-def get_generational_artifacts_path(
-    generation_number: int,
-    directory: pathlib.Path,
-) -> pathlib.Path:
-    return (directory / str(generation_number)).with_suffix(GENERATION_OUTPUT_EXTENSION)
-
-
 def save_generational_artifacts(
     generation_number: int,
     fittest: dict[cg.CompositeGenotype, gf.Fitness],
@@ -69,8 +61,8 @@ def save_generational_artifacts(
 
     serialized_gen_out = pickle.dumps(gen_out, protocol=pickle.HIGHEST_PROTOCOL)
 
-    output_dir.mkdir(parents=True, exist_ok=True)
-    path = get_generational_artifacts_path(generation_number, output_dir)
+    path = gge.paths.get_generation_output_path(output_dir, generation_number)
+    path.parent.mkdir(parents=True, exist_ok=True)
     path.write_bytes(serialized_gen_out)
 
     logger.info(
@@ -85,6 +77,6 @@ def load_generational_artifacts(path: pathlib.Path) -> GenerationOutput:
 
 
 def load_latest_generational_artifacts(output_dir: pathlib.Path) -> GenerationOutput:
-    paths = output_dir.glob(f"*{GENERATION_OUTPUT_EXTENSION}")
+    paths = output_dir.glob(f"*{gge.paths.GENERATION_OUTPUT_EXTENSION}")
     latest = max(paths, key=lambda path: int(path.stem))
     return load_generational_artifacts(latest)
