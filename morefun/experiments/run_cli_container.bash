8517b9f9896a0d2445b5fc331d3c693bbb012d0b
MirandaTZ <thiago.zafalon.miranda@gmail.com>
2023-04-03 12:44:07 -0300

    Remove gge references from run_cli_container.bash

--- morefun/experiments/run_cli_container.bash
+++ morefun/experiments/run_cli_container.bash
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # before running, set the following envinroment variables:
-# GGE_DATASETS_PATH      path to a directory with datasets
-# GGE_REPOSITORY_PATH   path to the root of the GGE git repository
+# MOREFUN_DATASETS_PATH   path to a directory with datasets
+# MOREFUN_REPOSITORY_PATH path to the root of the MOREFUN git repository
 
 # usage:
-# ./cli.bash <path-to-settings.toml> <gge-python-cli-commands>
+# ./cli.bash <path-to-settings.toml> <morefun-python-cli-commands>
 
 set -o errexit
 set -o nounset
 set -o xtrace
 set -o pipefail
 
-CODE_MOUNT_POINT=$(realpath "$GGE_REPOSITORY_PATH")
-DATASETS_MOUNT_POINT="$(realpath "$GGE_DATASETS_PATH")"
+CODE_MOUNT_POINT=$(realpath "$MOREFUN_REPOSITORY_PATH")
+DATASETS_MOUNT_POINT="$(realpath "$MOREFUN_DATASETS_PATH")"
 
 # ensuring directories exist so they are no created (as root) by docker
 if [ ! -d "$CODE_MOUNT_POINT" ]; then
@@ -41,10 +41,10 @@ docker run \
     --rm \
     --runtime=nvidia \
     --shm-size=8gb \
-    --workdir=/gge/code \
-    -v "$SETTINGS_MOUNT_POINT":/gge/settings.toml:ro \
-    -v "$DATASETS_MOUNT_POINT":/gge/datasets:ro \
-    -v "$CODE_MOUNT_POINT":/gge/code:ro \
-    -v "$OUTPUT_MOUNT_POINT":/gge/output \
-    mirandatz/gge:dev_env \
-    bash -c "source /venv/bin/activate && python -m gge.experiments.cli $*"
+    --workdir=/app/code \
+    -v "$SETTINGS_MOUNT_POINT":/app/settings.toml:ro \
+    -v "$DATASETS_MOUNT_POINT":/app/datasets:ro \
+    -v "$CODE_MOUNT_POINT":/app/code:ro \
+    -v "$OUTPUT_MOUNT_POINT":/app/output \
+    mirandatz/morefun:dev_env \
+    bash -c "source /venv/bin/activate && python -m morefun.experiments.cli $*"
