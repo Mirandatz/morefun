c39c74b6e8c9ac3353de10eabc2b825cd1ba4c48
Thiago Zafalon Miranda <thiago.miranda@daedalus-tech.com>
2024-12-29 16:08:33 -0300

    add tmp.bash

---
+++ scripts/tmp.bash
@@ -0,0 +1,4 @@
+./analyze_results.py \
+    -s /home/thiago/source/phd/morefun/persistent/experiments/cifar10_train_val_test/run_0/settings.yaml \
+    -d /home/thiago/source/phd/morefun/persistent/datasets/cifar10 \
+    -o /home/thiago/source/phd/morefun/persistent/experiments/cifar10_train_val_test/run_0/output
