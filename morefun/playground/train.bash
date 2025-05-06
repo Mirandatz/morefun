d6f0d79a162413a38afe6dcf5dda1bfde61542be
Thiago Zafalon Miranda <thiago.miranda@daedalus-tech.com>
2025-05-05 23:55:55 -0300

    add new train script

---
+++ morefun/playground/train.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -o errexit  # abort on nonzero exitstatus
+set -o nounset  # abort on unbound variable
+set -o pipefail # don't hide errors within pipes
+set -o xtrace   # echo commands as they are run
+
+python -m morefun.experiments.initialize_population -s persistent/scenario_single_objective/experiments/run_0/settings.yaml
+
+# scenario=single_objective
+
+# for i in {0..4}; do
+#     export exp_dir="/disk2/thiago/experiments/v2/${scenario}/run_${i}"
+    
+#     /home/thiago/source/morefun/scripts/initialize_population.py \
+#         -s "${exp_dir}/settings.yaml" \
+#         -d /disk2/thiago/datasets/cifar10_train_test \
+#         -o ${exp_dir}/output
+
+#     /home/thiago/source/morefun/scripts/evolve_population.py \
+#         -s "${exp_dir}/settings.yaml" \
+#         -d /disk2/thiago/datasets/cifar10_train_test \
+#         -o ${exp_dir}/output \
+#         --generations 30
+# done
+
+# for i in {0..4}; do
+#     export exp_dir="/disk2/thiago/experiments/v2/${scenario}/run_${i}"
+
+#     /home/thiago/source/morefun/scripts/analyze_results.py \
+#         -s "${exp_dir}/settings.yaml" \
+#         -d /disk2/thiago/datasets/cifar10_train_test \
+#         -o ${exp_dir}/output
+# done
